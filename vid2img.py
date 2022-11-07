bcb66b82232af2cb63bd217b69def06bafa18261
andyyoshikawa <yoshikawa0121@icloud.com>
2022-11-07 14:53:10 +0900

    Create vid2img.py

---
+++ vid2img.py
@@ -0,0 +1,37 @@
+import cv2
+import os
+import glob
+import tqdm
+
+
+def save_all_frames(vid_path, dir_path, ext='jpg'):
+    cap = cv2.VideoCapture(vid_path)
+
+    if not cap.isOpened():
+        return
+
+    os.makedirs(dir_path, exist_ok=True)
+    digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
+
+    n = 0
+
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            cv2.imwrite(f'{dir_path}/img_{str(n).zfill(digit)}', frame)
+            n += 1
+        else:
+            return
+
+
+if __name__ == '__main__':
+    vid_dir = ''
+    vid_ext = 'mp4'
+    img_dir = ''
+    img_ext = 'jpg'
+
+    files = glob.glob(f'{vid_dir}/.{vid_ext}')
+
+    for index, file in enumerate(tqdm(files)):
+        basedir = f'{img_dir}/vid_{str(index).zfill(5)}'
+        save_all_frames(file, basedir, ext=img_ext)
