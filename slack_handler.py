ebece8ce734cc25e5b0818cdaaeebb531d691601
andyyoshikawa0121 <yoshikawa0121@icloud.com>
2022-11-09 14:14:58 +0900

    Apply config_local.yaml

--- slack_handler.py
+++ slack_handler.py
@@ -35,101 +35,14 @@ def create_attachments(pretext, color, params):
   
   return json.dumps(data)
 
-def process_notify(time, index, modelname):
-  with open('./config.yaml') as f:
-    args = yaml.safe_load(f)
-
-    webhook_url = args['SlackHandler']['webhook_url']
-
-    data = json.dumps({
-      "attachments": [
-        {
-          "pretext": "Study Notification.",
-          "color": "#AAFFAA",
-          "fields": [
-            {
-              "title": "Parameters",
-              "short": False,
-            },
-            {
-              "value": f"Model: {modelname}",
-              "short": False,
-            },
-            {
-              "value": f"Iterations: {index}",
-              "short": False,
-            },
-            {
-              "value": f"Time: {time}",
-              "short": False,
-            },
-          ]
-        }
-      ]
-    })
-    slack(data, webhook_url)
-  
-def error_notify(error, modelname):
-  with open('./config.yaml') as f:
-    args = yaml.safe_load(f)
-
-    webhook_url = args['SlackHandler']['webhook_url']
-
-    data = json.dumps({
-      "attachments": [
-        {
-          "pretext": "Error occurred",
-          "color": "#E60033",
-          "fields": [
-            {
-              "title": "Error",
-              "short": False,
-            },
-            {
-              "value": f"Model: {modelname}",
-              "short": False,
-            },
-            {
-              "value": error,
-              "short": False,
-            }
-          ] 
-        }
-      ]
-    })
-    slack(data, webhook_url)
-
-def finish_notify(modelname):
-  with open('./config.yaml') as f:
-    args = yaml.safe_load(f)
-  
-  webhook_url = args['SlackHandler']['webhook_url']
-  
-  data = json.dumps({
-    "attachments": [
-      {
-        "pretext": "Study Finished",
-        "color": "#00BFE6",
-        "fields": [
-          {
-            "title": "Finish",
-            "short": False,
-          },
-          {
-            "value": f"Model: {modelname}",
-            "short": False,
-          },
-        ]
-      }
-    ]
-  })
-  slack(data, webhook_url)
-
 
 if __name__ == '__main__':
     with open('./config.yaml') as f:
         args = yaml.safe_load(f)
+    with open('./config_local.yaml') as fl:
+      args_local = yaml.safe_load(fl)
 
+    args.update(args_local)
     webhook_url = args['SlackHandler']['webhook_url']
 
     # requests.post(webhook_url, data=json.dumps({
@@ -140,32 +53,32 @@ if __name__ == '__main__':
     # error_notify('Segmentation Fault', "MoCoGAN-HD")
     # finish_notify("MoCoGAN-HD")
 
-    # post_params = [
-    #   {
-    #     "is_title": True,
-    #     "name": "Study Finished",
-    #     "value": "",
-    #   },
-    #   {
-    #     "is_title": False,
-    #     "name": "Model",
-    #     "value": "MoCoGAN-HD",
-    #   },
-    #   {
-    #     "is_title": False,
-    #     "name": "Iterations",
-    #     "value": "50000",
-    #   },
-    #   {
-    #     "is_title": False,
-    #     "name": "FID",
-    #     "value": "20.3",
-    #   },
-    #   {
-    #     "is_title": False,
-    #     "name": "Time",
-    #     "value": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    #   }
-    # ]
-    # data = create_attachments("Study Finish Notification", "#00BFE6", post_params)
-    # res = slack(data, webhook_url)
+    post_params = [
+      {
+        "is_title": True,
+        "name": "Study Finished",
+        "value": "",
+      },
+      {
+        "is_title": False,
+        "name": "Model",
+        "value": "MoCoGAN-HD",
+      },
+      {
+        "is_title": False,
+        "name": "Iterations",
+        "value": "50000",
+      },
+      {
+        "is_title": False,
+        "name": "FID",
+        "value": "20.3",
+      },
+      {
+        "is_title": False,
+        "name": "Time",
+        "value": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+      }
+    ]
+    data = create_attachments("Study Finish Notification", "#00BFE6", post_params)
+    res = slack(data, webhook_url)
